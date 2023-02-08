ee916a0b7c6383bd797f3af036d2d7a9adb9a0a9
Jullianyy <123980122+Jullianyy@users.noreply.github.com>
2023-02-08 16:03:19 -0300

    Add files via upload

---
+++ cadfornecedor.php
@@ -0,0 +1,16 @@
+<?php
+include "conexao.php";
+/*variável php tem que começar com $ recebe a variavel do formulário*/
+$razaosocial = $_POST['razaosocial'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$sql = "insert into fornecedor values (null, '".$razaosocial."', '".$email."', '".$senha."')";
+
+if(mysqli_query($conexao, $sql)) {
+        echo "Gravado com sucesso!";
+    }else{
+        echo "Erro ao gravar!";
+}
+mysqli_close($conexao);
+?>
+<br>
